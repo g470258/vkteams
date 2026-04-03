19b6e3608142a47db3ff8e934043ec690a749afa
Tatarinov Gleb <61633115+g470258@users.noreply.github.com>
2026-04-03 12:19:17 +0300

    Delete vkteams directory

--- vkteams/helpers.py
+++
@@ -1,8 +0,0 @@
-"""Helper functions for VK Teams integration."""
-
-from .const import SIGNAL_UPDATE_EVENT
-
-
-def signal(bot_id: str) -> str:
-    """Define signal name."""
-    return f"{SIGNAL_UPDATE_EVENT}_{bot_id}"
